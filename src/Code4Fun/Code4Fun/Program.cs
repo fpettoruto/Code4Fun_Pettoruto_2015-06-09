767fb5d793b01aded424494f3640a38de09117f1
fpettoruto <fabrizio.pettoruto@gmail.com>
2015-06-10 17:20:44 +0200

    console
    added code in console for execute

--- src/Code4Fun/Code4Fun/Program.cs
+++ src/Code4Fun/Code4Fun/Program.cs
@@ -10,6 +10,36 @@ namespace Code4Fun
     {
         static void Main(string[] args)
         {
+            try
+            {
+                Console.WriteLine("Insert a max number of element:");
+                var  maxNumber=0;
+                var input = Console.ReadLine();
+                int.TryParse(input,  out maxNumber);
+                var rad = new RandomArrayDistinct(maxNumber);
+                rad.PopulateArray();
+                for (int i = 0; i < rad.DistinctArray.Length; i++)
+                {
+                    Console.WriteLine(i + "-->\t" + rad.DistinctArray[i]);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+
+            }
+            finally
+            {
+                Console.WriteLine("Press any key");
+                Console.WriteLine("\n");
+                Console.ReadKey();
+            }
+
+            //Console.ReadKey();
+
+
         }
+
     }
 }
