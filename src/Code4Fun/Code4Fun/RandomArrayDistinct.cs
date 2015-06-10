df6c3edba154b69e93bb2ef83ea71ad5dc3f688c
fpettoruto <fabrizio.pettoruto@gmail.com>
2015-06-10 14:55:55 +0200

    added tests
    first step red test

---
+++ src/Code4Fun/Code4Fun/RandomArrayDistinct.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Code4Fun
+{
+    class RandomArrayDistinct
+    {
+        private int maxElement;
+        private int[] IntIsUsed;
+
+        public int[] DistinctArray { get; private set; }
+
+        public RandomArrayDistinct(int maxElememt)
+        {
+            maxElement = maxElememt;
+            IntIsUsed = new int[maxElememt];
+            DistinctArray = new int[maxElememt];
+        }
+        public void PopulateArray()
+        {
+            throw new NotImplementedException();
+        }
+
+      
+    }
+}
---
+++ src/Code4Fun/Code4Fun/RandomArrayDistinctTest.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+
+namespace Code4Fun
+{
+    [TestFixture]
+    class RandomArrayDistinctTest
+    {
+        private const int MAX_ELEMENT = 100;
+        private RandomArrayDistinct sut;
+
+        [SetUp]
+        public void Setup()
+        {
+            sut = new RandomArrayDistinct(MAX_ELEMENT);
+        }
+
+        [Test]
+        public void ShouldArrayIsNotEmpty()
+        {
+            Assert.IsNotEmpty(sut.DistinctArray);
+        }
+
+        [Test]
+        public void ShouldArrayContainsMinValue()
+        {
+            sut.PopulateArray();
+            Assert.That(sut.DistinctArray.Contains(1));
+        }
+
+        [Test]
+        public void ShouldArrayContainsMaxValue()
+        {
+            sut.PopulateArray();
+            Assert.That(sut.DistinctArray.Contains(MAX_ELEMENT));
+        }
+
+        [Test]
+        public void ShouldArrayNotHasDuplicates()
+        {
+            sut.PopulateArray();
+            Assert.That(HasDuplicates(sut.DistinctArray) == false);
+        }
+
+        private bool HasDuplicates(int[] array)
+        {
+            Array.Sort(array);
+            for (var i = 0; i < array.Length - 1; i++)
+            {
+                if (array[i] == array[i + 1]) return true;
+            }
+            return false;
+        }
+
+    }
+}
